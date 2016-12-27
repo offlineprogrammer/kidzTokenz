411616eb5328a132a4114f64c36e221919339753
Offline Programmer <mmalaka@gmail.com>
2016-12-27 22:34:15 +0000

    formGroup

--- src/pages/home/add-kid-modal.ts
+++ src/pages/home/add-kid-modal.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Validators, FormBuilder,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'page-addKidModal',
   templateUrl: 'add-kid-modal.html'
 })
 export class AddKidModal {
- 
+ form: FormGroup;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private formBuilder: FormBuilder
+  ) {
+
+      this.form = this.formBuilder.group({
+      kidName: ['', Validators.required],
+     
+    });
 
   }
+
+ 
+  
 
   
 
