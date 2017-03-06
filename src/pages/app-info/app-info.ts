cc1c4211c50a5db2cd31bc347cd58e5fe2e8169b
Offline Programmer <mmalaka@gmail.com>
2017-03-06 13:59:53 +0000

    @statment

--- src/pages/app-info/app-info.ts
+++ src/pages/app-info/app-info.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams,ViewController } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { AppRate } from 'ionic-native';
+import { Platform } from 'ionic-angular';
 
 /*
   Generated class for the AppInfo page.
@@ -12,16 +14,29 @@ import { NavController, NavParams,ViewController } from 'ionic-angular';
   templateUrl: 'app-info.html'
 })
 export class AppInfoPage {
+  appRate: any = AppRate;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private viewController: ViewController) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewController: ViewController, public platform: Platform) {
+    this.platform.ready().then(
+      () => this.appRate.preferences.storeAppURL = {
+        ios: '1150112049',
+        android: 'market://details?id=com.offlineprogrammer.KidzTokenz'
+      }
+    )
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AppInfoPage');
   }
 
 
-   close() {
+  close() {
     this.viewController.dismiss();
+  }
+
+  rateApp() {
+    AppRate.promptForRating(true);
   }
 
 
